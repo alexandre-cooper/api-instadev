e7e62b1387e953bf7f21feee669385584109ac94
Alexandre Brasil <alexandre@lekko.com.br>
2024-05-31 12:33:47 -0400

    feat: models users

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
-
+const UserModel = require("./apps/models/Users");
 const routes = new Router();
 
 routes.get("/health", (req, res) => {
   res.status(200).json("Servidor on");
+});
+
+routes.get("/users", async (req, res) => {
+  const allUsers = await UserModel.findAll();
+  res.send({ users: allUsers });
 });
 
 module.exports = routes;
